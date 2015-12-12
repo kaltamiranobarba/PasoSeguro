47d8a0b0939499ffc03697170fbfc0da66996de2
Kenny Altamirano Barba <altamiranobarba@outlook.com>
2015-12-12 18:25:08 -0500

    Login, signup, conexion bbdd

--- PasoSeguro/app/src/main/java/com/example/altam/pasoseguro/LoginActivity.java
+++ PasoSeguro/app/src/main/java/com/example/altam/pasoseguro/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.example.altam.pasoseguro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.*;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
-    Button b_enter;
+    Button btn_loginEnter;
+    EditText et_loginUser, et_loginPass;
+    String passRetrieved, userRetrieved, emailRetrieved, ageRetrieved;
+    Boolean retrieved =false;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +27,59 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
-        b_enter = (Button) findViewById(R.id.B_enter);
-        b_enter.setOnClickListener(this);
+        Parse.initialize(this, "gtUrodDDuFstbcyGN9gkyBGTDliHmtk5ADsMzGjH", "3rKd6641H0J5ILak2yPUiIBmOjsQxtc3PlYODIWG");
+        btn_loginEnter = (Button) findViewById(R.id.btn_loginEnter);
+        et_loginPass = (EditText) findViewById(R.id.et_loginPass);
+        et_loginUser = (EditText) findViewById(R.id.et_loginUser);
+        btn_loginEnter.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        String user, pass;
+        user = et_loginUser.getText().toString();
+        pass= et_loginPass.getText().toString();
 
+            if (login(user, pass)) {
+                Toast.makeText(getApplicationContext(), "LOGIN EXITOSO!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(LoginActivity.this, MapActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(), "Usuario o clave erróneo", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+
+
+
+    public boolean login(String user, String pass){
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+        query.whereEqualTo("user",user);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (object == null) {
+                    Log.d("objeto","NO SE RECUPERO");
+
+                } else {
+                    userRetrieved = object.getString("User");
+                    passRetrieved = object.getString("pass");
+                    emailRetrieved = object.getString("email");
+                    ageRetrieved = object.getString("age");
+                    retrieved = true;
+                }
+            }
+
+        });
+        if(retrieved){
+            if(pass.equals(passRetrieved))
+                return true;
+            else
+                return false;
+        }
+        return false;
     }
+
+
 }
--- PasoSeguro/app/src/main/java/com/example/altam/pasoseguro/SignupActivity.java
+++ PasoSeguro/app/src/main/java/com/example/altam/pasoseguro/SignupActivity.java
@@ -1,29 +1,63 @@
 package com.example.altam.pasoseguro;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
 
-public class SignupActivity extends Activity {
+public class SignupActivity extends Activity implements View.OnClickListener {
+    Button btn_signup;
+    EditText et_signupUser, et_signupPass, et_signupEmail, et_signupAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        Parse.enableLocalDatastore(this);
 
-        Parse.initialize(this);
+        Parse.initialize(this, "gtUrodDDuFstbcyGN9gkyBGTDliHmtk5ADsMzGjH","3rKd6641H0J5ILak2yPUiIBmOjsQxtc3PlYODIWG" );
 
-       ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
 
+
+
+
+
+        et_signupUser = (EditText) findViewById(R.id.et_signupUser);
+        et_signupPass = (EditText) findViewById(R.id.et_signupPass);
+        et_signupEmail = (EditText) findViewById(R.id.et_signupEmail);
+        et_signupAge = (EditText) findViewById(R.id.et_signupAge);
+
+        btn_signup = (Button) findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(this);
     }
 
 
+
+    @Override
+    public void onClick(View v) {
+        String user, pass, email, age;
+
+        user = et_signupUser.getText().toString();
+        pass = et_signupPass.getText().toString();
+        email = et_signupEmail.getText().toString();
+        age = et_signupAge.getText().toString();
+        ParseObject po = new ParseObject("User");
+        po.put("user",user);
+        po.put("pass", pass);
+        po.put("email", email);
+        po.put("age", age);
+        po.saveInBackground();
+
+        Toast.makeText(getApplicationContext(), "Usuario registrado!",
+            Toast.LENGTH_LONG).show();
+
+    }
 
 
 }
