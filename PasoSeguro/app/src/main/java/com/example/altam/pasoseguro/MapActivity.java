b2a254bc1fa353d45ce92bef0bdf7800c8060fad
Jose Romero <joanrome@espol.edu.ec>
2015-12-15 00:18:10 -0500

    Material Design implementado en antalla de mapa

--- PasoSeguro/app/src/main/java/com/example/altam/pasoseguro/MapActivity.java
+++ PasoSeguro/app/src/main/java/com/example/altam/pasoseguro/MapActivity.java
@@ -1,7 +1,16 @@
 package com.example.altam.pasoseguro;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,18 +19,84 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity extends/* FragmentActivity*/ AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private DrawerLayout mDrawerLayout;
+    private TextView drawerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_map_final);
+
+        //mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                Toast.makeText(MapActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        drawerText = (TextView) findViewById(R.id.drawer_header_textview);
+        drawerText.setText("joseromero");
+
+
+        /*setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_filter:
+                return true;
+            case R.id.fast_filter_item_anio:
+                item.setChecked(true);
+                return true;
+            case R.id.fast_filter_item_mes:
+                item.setChecked(true);
+                return true;
+            case R.id.fast_filter_item_semana:
+                item.setChecked(true);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
