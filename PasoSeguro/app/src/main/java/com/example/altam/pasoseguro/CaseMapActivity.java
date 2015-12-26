d9212299b51d8d8c327814400fc8eb58e8d4ab31
Jose Romero <joanrome@espol.edu.ec>
2015-12-25 21:30:14 -0500

    Arreglado el problema del scroll en CaseMapActivity

--- PasoSeguro/app/src/main/java/com/example/altam/pasoseguro/CaseMapActivity.java
+++ PasoSeguro/app/src/main/java/com/example/altam/pasoseguro/CaseMapActivity.java
@@ -2,6 +2,10 @@ package com.example.altam.pasoseguro;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,16 +16,50 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class CaseMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    ScrollView mainScrollView;
+    ImageView transparentImageView;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case_map);
+
+        mainScrollView = (ScrollView) findViewById(R.id.sv_container);
+        transparentImageView = (ImageView) findViewById(R.id.transparent_image);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
+        transparentImageView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        mainScrollView.requestDisallowInterceptTouchEvent(true);
+                        // Disable touch on transparent view
+                        return false;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        mainScrollView.requestDisallowInterceptTouchEvent(false);
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        mainScrollView.requestDisallowInterceptTouchEvent(true);
+                        return false;
+
+                    default:
+                        return true;
+                }
+            }
+        });
     }
 
 
